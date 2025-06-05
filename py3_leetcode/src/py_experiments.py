e0764f5f64de91ea7b9df7858042b32588265260
Anton Pegushin <anton.pegushin@gmail.com>
2025-06-05 11:58:22 -0700

    Trying uv as package and project manager.

---
+++ py3_leetcode/src/py_experiments.py
@@ -0,0 +1,14 @@
+from typing import List, Optional
+import time
+import heapq
+
+def fstring_print():
+    a, b = 10, 15
+    print(f'{a + b = }')
+
+def var_avail():
+    print('starting var_avail')
+
+
+if __name__ == '__main__':
+    var_avail()
