a64de3d77d2545d77e621573e535d7043b653690
Anton Pegushin <anton.pegushin@gmail.com>
2025-06-17 00:15:00 -0700

    Shortest path through game board prev interview problem - code and test.

--- py3_leetcode/tests/test_prev_interviews.py
+++ py3_leetcode/tests/test_prev_interviews.py
@@ -10,3 +10,17 @@ import pytest
                         ])
 def test_cd_dir(curwd, cdto, result):
     assert mcd.cd_dir(curwd, cdto) == result
+
+@pytest.mark.parametrize('board, moves_list',
+                        [([[0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 1, 0, 0, 1, 0],
+                           [0, 0, 1, 0, 1, 1, 0],
+                           [0, 0, 1, 0, 1, 0, 1],
+                           [1, 1, 1, 0, 0, 0, 0]],
+                           [(0, 0), (0, 1), (0, 2), (0, 3), (1, 3), (2, 3),
+                            (3, 3), (4, 3), (4, 4), (4, 5), (4, 6)]),
+                        ])
+def test_shortestBridge(board, moves_list):
+    res = mcd.shortestPath(board)
+    print(res)
+    assert res == moves_list
